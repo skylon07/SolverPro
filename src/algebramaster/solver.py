df2bde29cc6fab14f0ab0648e160f21e7c84b43d
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-22 11:57:34 -0700

    Finish _SympySolveTools
    Other important notes:
    - Solution inner class is now public
    - One TODO added -- need to create NumericSymbol structure class

--- src/algebramaster/solver.py
+++ src/algebramaster/solver.py
@@ -59,48 +59,86 @@ class _SympySolveTools:
     class _SolutionTypes:
         @property
         def NORMAL(self):
-            """ex. FiniteSet({3, a + 2, b * c})"""
             return "NORMAL"
 
         @property
         def EMPTY(self):
-            """ex. EmptySet() == {}"""
             return "EMPTY"
 
         @property
         def COMPLEXES(self):
-            """ex. TODO"""
             return "COMPLEXES"
 
         @property
         def COMPLEMENT(self):
-            """ex. TODO"""
             return "COMPLEMENT"
 
         @property
         def CONDITIONAL(self):
-            """ex. TODO"""
             return "CONDITIONAL"
     
     solutionTypes = _SolutionTypes()
 
     @classmethod
     def solveSet(cls, expr, atom):
-        pass # TODO
+        if isNumeric(atom):
+            # solving for variables in exponents leads to errors with no clean fixes,
+            # so we just ignore them by converting them to symbols first
+            atomSub = sympy.Symbol(":NUMERIC {}:".format(atom))
+            exprWithNumericSubbed = cls._substituteNumericInBadTerms(atom, atomSub, expr)
+            solution = cls._solveSetForAtom(exprWithNumericSubbed, atom)
+            solution.reverseSubstitutionOfNumeric({atomSub: atom})
+            return solution
+        else:
+            return cls._solveSetForAtom(expr, atom)
 
     @classmethod
     def _substituteNumericInBadTerms(cls, numericAtom, numericAtomSub, expr):
-        pass # TODO
+        isBadExpr = expr.is_Pow
+        if isBadExpr:
+            return expr.subs(numericAtom, numericAtomSub)
+        elif expr.is_Atom:
+            return expr
+        else:
+            # this is a neat way to traverse expression trees as given by
+            # https://docs.sympy.org/latest/tutorials/intro-tutorial/manipulation.html#recursing-through-an-expression-tree
+            expr: sympy.Expr = expr
+            return expr.func(*(
+                cls._substituteNumericInBadTerms(numericAtom, numericAtomSub, term)
+                for term in expr.args
+            ))
 
     @classmethod
-    def _solveSetForNonNumeric(cls, expr, atom):
-        pass # TODO (and assert atom is not numeric)
+    def _solveSetForAtom(cls, expr, atom):
+        solutionSet = sympy.solveset(expr, atom)
+        return cls.Solution(solutionSet)
     
     @classmethod
     def _interpretSolution(cls, solution):
-        pass # TODO
+        types = cls.solutionTypes
+        if solution.type is types.NORMAL:
+            return solution.set
+        elif solution.type is types.COMPLEXES:
+            # this means a variable can be any value and still hold true in the relation,
+            # which ultimately means the relation provided no new information for the symbol
+            return None
+        elif solution.type is types.EMPTY:
+            # similar to the above case, except we ended up with some kind of
+            # invalid/impossible relation, a "4 = 0 case" (say, 1 + 3*(b + 1)/(b + 1))
+            return set()
+        elif solution.type is types.COMPLEMENT:
+            if solution.set.args[0] is not sympy.Complexes:
+                # this happens when the solver is forced to put a variable in the denominator of a fraction;
+                # this generates a solution with (calculus) "holes", which can be ignored
+                # (happens in cases like solving a/b - 4 for b)
+                return solution.set.args[0]
+            elif solution.set.args[0] is sympy.Complexes:
+                # this happens in cases similar to the above, except sort of reversed;
+                # here, the solver found that anything is possible *except* for certain
+                # "holes", which are actually the solutions (TODO: example case needed)
+                return solution.set.args[1]
     
-    class _Solution:
+    class Solution:
         def __init__(self, solutionSet):
             self._solutionSet = solutionSet
             self._type = self._getSetType(solutionSet)
@@ -128,8 +166,43 @@ class _SympySolveTools:
             else:
                 raise NotImplementedError("Solution ran into an unconsidered type scenario")
 
-        def reverseSubstitutionOfNumeric(self, subsDict):
+        def reverseSubstitutionOfNumeric(self, subsDict: dict):
             self._solutionSet = self._performSubsOnSet(self._solutionSet, subsDict)
 
         def _performSubsOnSet(self, sympySet, subsDict):
-            pass # TODO
+            types = _SympySolveTools.solutionTypes
+            setType = self._getSetType(sympySet)
+            if setType is types.NORMAL:
+                returnSet = sympy.FiniteSet(*(
+                    expr.subs(subsDict)
+                    for expr in sympySet
+                ))
+            elif setType is types.EMPTY:
+                # there is nothing to substitute in the empty set {}
+                returnSet = sympySet
+            elif setType is types.COMPLEXES:
+                # sort of like the above case, except it's everything
+                returnSet = sympySet
+            elif setType is types.COMPLEMENT:
+                (mainSet, complementSet) = sympySet.args
+                mainSetSubbed = self._performSubsOnSet(mainSet, subsDict)
+                complementSetSubbed = self._performSubsOnSet(complementSet, subsDict)
+                return sympy.Complement(mainSetSubbed, complementSetSubbed)
+            elif setType is types.CONDITIONAL:
+                (symbol, eqCondition, baseSet) = sympySet.args
+                assert type(eqCondition) is sympy.Eq, "sympy ConditionSet was assumed to always have Eq() as the second argument"
+                (eqLeft, eqRight) = eqCondition.args
+                symbolSubbed = symbol.subs(subsDict)
+                if isNumeric(symbolSubbed):
+                    # ConditionSets really don't like numerics as their first arg... this gets around that
+                    # (of course, this means the extractor will need to convert it back later...)
+                    # TODO: create NumericSymbol class
+                    symbolSubbed = NumericSymbol(symbolSubbed)
+                eqConditionSubbed = sympy.Eq(eqLeft.subs(subsDict), eqRight.subs(subsDict))
+                baseSetSubbed = self._performSubsOnSet(baseSet, subsDict)
+                return sympy.ConditionSet(symbolSubbed, eqConditionSubbed, baseSetSubbed)
+            else:
+                raise NotImplementedError("Tried to substitute the set for an unconsidered solution type")
+            
+            assert not any(symbolKey in expr.atoms() for expr in returnSet for symbolKey in subsDict)
+            return returnSet
