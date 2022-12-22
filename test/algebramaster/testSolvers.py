743049ab58047593770a5fd82fddccda468d3d07
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-12-22 11:57:39 -0700

    Write tests for _SympySolveTools

---
+++ test/algebramaster/testSolvers.py
@@ -0,0 +1,33 @@
+import sympy
+
+from src.algebramaster.solver import AlgebraSolver, _SympySolveTools
+
+
+(a, b, c, two) = sympy.symbols("a, b, c, two")
+
+
+class SympySolveToolsTester:
+    def testReverseSubstituteForBadNumerics(self):
+        expr = a + b**two
+        equation = 2 - expr
+        solution = _SympySolveTools.Solution(sympy.solveset(equation, 2))
+        solution.reverseSubstitutionOfNumeric({two: 2})
+        assert solution.set == sympy.FiniteSet(a + b**2)
+
+    def testSolveSetPerformsSolving(self):
+        expr = a + b*c - 4
+        
+        actualSolution1 = {4}
+        toolsSolution1 = _SympySolveTools.solveSet(expr, a + b*c).set
+        assert toolsSolution1 == actualSolution1
+
+        actualSolution2 = {b*c}
+        toolsSolution2 = _SympySolveTools.solveSet(expr, -(a - 4)).set
+        assert toolsSolution2 == actualSolution2
+
+    def testSolveSetSolvesForNumerics(self):
+        expr = a + b**2 - 2
+        
+        actualSolution1 = {a + b**2}
+        toolsSolution1 = _SympySolveTools.solveSet(expr, 2).set
+        assert toolsSolution1 == actualSolution1
