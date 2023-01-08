e2383bf9553d6c94763a3e016f04f27fbc4f68f8
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-01-07 21:12:39 -0700

    Add some simple equality tests for SubDict and SubDictList

---
+++ test/algebramaster/testStructures.py
@@ -0,0 +1,21 @@
+import sympy
+
+from src.algebramaster.structures import *
+
+
+(a, b) = sympy.symbols("a, b")
+
+
+class SubDictTester:
+    def testSubDictCanEqualDict(self):
+        origDict = {a: 2, b: 5}
+        subDict = SubDict(origDict)
+        
+        assert subDict == origDict
+
+class SubDictListTester:
+    def testSubDictListCanEqualListOfDicts(self):
+        origList = [{a: 2, b: 5}, {a: -2, b: -5}]
+        subDictList = SubDictList([SubDict(origDict) for origDict in origList])
+
+        assert subDictList == origList        
