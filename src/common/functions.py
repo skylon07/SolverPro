5c60e5d4b5eea20e48c4983ddff3a4252291b18f
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-01-14 11:15:14 -0700

    Add new emplaceInto() function

--- src/common/functions.py
+++ src/common/functions.py
@@ -15,3 +15,11 @@ def first(iterable, default=_firstNoDefault):
             raise ValueError("Iterable contained no elements (and no default value was given)")
         else:
             return default
+
+def emplaceInto(iterable1, iterable2):
+    if isinstance(iterable2, dict):
+        iterable2.update(iterable1)
+    elif isinstance(iterable2, list):
+        iterable2.extend(iterable1)
+    else:
+        raise NotImplementedError("Unconsidered emplacement type")
