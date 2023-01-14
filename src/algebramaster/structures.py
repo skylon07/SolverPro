9c49a7dcac690295b7900c4c2b6796fb26f0e864
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-01-14 11:12:28 -0700

    Move functions into common functions

--- src/algebramaster/structures.py
+++ src/algebramaster/structures.py
@@ -162,22 +162,3 @@ def isNumeric(obj):
         return True
     except TypeError:
         return False
-
-
-def iterDifference(iter1, iter2):
-    for item in iter1:
-        if item not in iter2:
-            yield item
-
-_firstNoDefault = object()
-def first(iterable, default=_firstNoDefault):
-    try:
-        for item in iterable:
-            firstItem = item
-            # because we...
-            return firstItem
-    except StopIteration:
-        if default is _firstNoDefault:
-            raise ValueError("Iterable contained no elements (and no default value was given)")
-        else:
-            return default
---
+++ src/common/functions.py
@@ -0,0 +1,17 @@
+def iterDifference(iter1, iter2):
+    for item in iter1:
+        if item not in iter2:
+            yield item
+
+_firstNoDefault = object()
+def first(iterable, default=_firstNoDefault):
+    try:
+        for item in iterable:
+            firstItem = item
+            # because we...
+            return firstItem
+    except StopIteration:
+        if default is _firstNoDefault:
+            raise ValueError("Iterable contained no elements (and no default value was given)")
+        else:
+            return default
