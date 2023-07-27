b7e3cca262e3a3a69398c4c9a91bf749717c09a0
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-07-26 23:59:29 -0600

    Implement solver and some tests

---
+++ src/algebrasolver/solver.py
@@ -0,0 +1,104 @@
+from typing import Iterable, Generator, Any
+
+import sympy
+
+from src.common.functions import first, iterDifference
+
+
+class AlgebraSolver:
+    def __init__(self):
+        # a list of relational expressions with an implied equality to zero
+        self._recordedRelations: list[sympy.Expr] = list()
+        # a mapping of a variable to its potential values (like var = ±2)
+        self._symbolValues: dict[sympy.Symbol, sympy.FiniteSet] = dict()
+
+    def recordRelation(self, leftExpr: sympy.Expr, rightExpr: sympy.Expr):
+        # TODO: check for contradictions
+        self._recordedRelations.append(self.toRelation(leftExpr, rightExpr))
+        self._inferSymbolValuesFromRelations()
+
+    def toRelation(self, leftExpr: sympy.Expr, rightExpr: sympy.Expr):
+        # relation = leftExpr == rightExpr
+        relation = leftExpr - rightExpr # == 0
+        return relation
+
+    def getSymbolValues(self, symbol: sympy.Symbol):
+        return self._symbolValues.get(symbol)
+    
+    def getRelationsWithSymbol(self, symbol: sympy.Symbol):
+        return [
+            relation
+            for relation in self._recordedRelations
+            if symbol in relation.free_symbols
+        ]
+    
+    def substituteKnownsFor(self, relation: sympy.Expr):
+        for symbolValueCombination in self._generateSymbolValueCombinations():
+            relationWithKnownsSubbed: sympy.Expr = relation.subs(symbolValueCombination)
+            yield relationWithKnownsSubbed
+
+    def _inferSymbolValuesFromRelations(self):
+        anySymbolsUpdated = False
+        for relationExpr in self._recordedRelations:
+            relationListOfSubbedKnowns = self.substituteKnownsFor(relationExpr)
+            relationsWithSingleUnknown = self._filterRelationsWithMultiSymbols(relationListOfSubbedKnowns)
+            symbolValuePairs = self._solveRelationsForSingleUnknown(relationsWithSingleUnknown)
+            (symbol, solutionSet) = self._condenseSymbolValuePairs(symbolValuePairs)
+            if symbol is not None:
+                self._symbolValues[symbol] = solutionSet
+                anySymbolsUpdated = True
+            
+        if anySymbolsUpdated:
+            self._inferSymbolValuesFromRelations()
+
+    def _filterRelationsWithMultiSymbols(self, relations: Iterable[sympy.Expr]):
+        for relation in relations:
+            hasOneUnknown = len(relation.free_symbols) == 1
+            if hasOneUnknown:
+                yield relation
+
+    def _solveRelationsForSingleUnknown(self, relations: Iterable[sympy.Expr]):
+        for relation in relations:
+            assert(len(relation.free_symbols) == 1), "Relation had more than one unknown symbol to solve for"
+            unknownSymbol = first(relation.free_symbols)
+            solutionSet = sympy.solveset(relation, unknownSymbol)
+            yield (unknownSymbol, solutionSet)
+
+    def _condenseSymbolValuePairs(self, symbolValuePairs: Iterable[tuple[sympy.Symbol, sympy.FiniteSet]]):
+        if __debug__:
+            symbolValuePairs = tuple(symbolValuePairs)
+            if len(symbolValuePairs) > 0:
+                symbolSolved = symbolValuePairs[0][0]
+                allSymbolsTheSame = all(symbol == symbolSolved for (symbol, value) in symbolValuePairs)
+                assert allSymbolsTheSame, "Substitutions in relation led to different unknown variables"
+
+        symbol = None
+        totalSolutionSet = sympy.FiniteSet()
+        for (symbol, solutionSet) in symbolValuePairs:
+            totalSolutionSet += solutionSet
+        return (symbol, totalSolutionSet)      
+
+    def _generateSymbolValueCombinations(self, currSymbolValueCombination: dict[sympy.Symbol, sympy.Basic] = None) -> Generator[dict[sympy.Symbol, sympy.Basic], Any, None]:
+        if currSymbolValueCombination is None:
+            currSymbolValueCombination = dict()
+
+        symbolToInclude = first(iterDifference(self._symbolValues, currSymbolValueCombination), None)
+        noSymbolsLeftToInclude = symbolToInclude is None
+        if noSymbolsLeftToInclude:
+            finishedCombination = currSymbolValueCombination
+            yield finishedCombination
+        else:
+            for currValue in self._symbolValues[symbolToInclude]:
+                # overwritten to save on memory (instead of copying and creating a bunch of dicts)
+                currSymbolValueCombination[symbolToInclude] = currValue
+                for finishedCombination in self._generateSymbolValueCombinations(currSymbolValueCombination):
+                    yield finishedCombination
+                # have to pop to allow re-including in repeated calls from parent
+                # (aka have to pop so iterDifference() doesn't ignore it thinking it's already handled)
+                currSymbolValueCombination.pop(symbolToInclude)
+
+
+class NoSymbolsError(Exception):
+    def __init__(self, relation: sympy.Expr):
+        super().__init__(f"No symbols found in relation `{relation}`")
+        self.relation = relation
---
+++ test/algebrasolver/testSolver.py
@@ -0,0 +1,66 @@
+import sympy
+
+from src.algebrasolver.solver import AlgebraSolver
+
+(a, b, c, d) = sympy.symbols("a, b, c, d")
+
+class AlgebraSolverTester:
+    def testSolverConvertsRelations(self):
+        solver = AlgebraSolver()
+        relation = solver.toRelation(a, b)
+        assert relation == a - b, \
+            "Solver did not correctly convert two expressions to single expression equal to zero"
+    
+    def testSolverRecordsRelations(self):
+        solver = AlgebraSolver()
+        solver.recordRelation(2*a, b + c)
+        assert solver._recordedRelations == [2*a - (b + c)], \
+            "Solver did not record relation as a single expression equal to zero"
+
+    def testSolverInfersSingleVariableValues(self):
+        solver1 = AlgebraSolver()
+        solver1.recordRelation(a, 4)
+        assert solver1._symbolValues == {a: {4}}, \
+            "Solver did not record single variable value from equality"
+        
+        solver2 = AlgebraSolver()
+        solver2.recordRelation(b, 5)
+        solver2.recordRelation(3, c)
+        assert solver2._symbolValues == {b: {5}, c: {3}}, \
+            "Solver did not record multiple variables from variable-value equalities"
+        
+    def testSolverSubstitutesKnownVariables(self):
+        solver = AlgebraSolver()
+        solver.recordRelation(a, 4)
+        solver.recordRelation(b, 3)
+        solver.recordRelation(c, 7)
+        assert list(solver.substituteKnownsFor(a)) == [4], \
+            "Solver did not correctly substitute single variable expression"
+        assert list(solver.substituteKnownsFor(a + b)) == [7], \
+            "Solver did not correctly substitute double variable expression"
+        assert list(solver.substituteKnownsFor(a**(b - 1) - c)) == [9], \
+            "Solver did not correctly substitute complicated expression"
+        assert list(solver.substituteKnownsFor(a*c + b*d)) == [3*d + 28], \
+            "Solver did not correctly substitute expression with leftover variable"
+
+    def testSolverInfersVariableSolutions(self):
+        solver1 = AlgebraSolver()
+        solver1.recordRelation(2*b, 8)
+        assert solver1._symbolValues == {b: {4}}
+
+        solver2 = AlgebraSolver()
+        solver2.recordRelation(9, a**2)
+        assert solver2._symbolValues == {a: {3, -3}}, \
+            "Solver did not correctly solve single variable relation"
+
+        solver3 = AlgebraSolver()
+        solver3.recordRelation(a, 2*b)
+        solver3.recordRelation(b, 5)
+        assert solver3._symbolValues == {a: {10}, b: {5}}, \
+            "Solver did not correctly infer value of second related variable from the first"
+        
+        solver4 = AlgebraSolver()
+        solver4.recordRelation(a**2, 4)
+        solver4.recordRelation(a + b, 5)
+        assert solver4._symbolValues == {a: {2, -2}, b: {3, 7}}, \
+            "Solver did not correctly infer multiple values from relation with multiple outcomes"
