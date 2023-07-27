b5dd17bf9061a6e12c7c3b1862d29f2c6cf45285
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-07-26 23:59:20 -0600

    Improve `first()` default handling

--- src/common/functions.py
+++ src/common/functions.py
@@ -4,17 +4,18 @@ def iterDifference(iter1, iter2):
             yield item
 
 _firstNoDefault = object()
-def first(iterable, default=_firstNoDefault):
+def first(iterable, default = _firstNoDefault):
     try:
         for item in iterable:
             firstItem = item
             # because we...
             return firstItem
     except StopIteration:
-        if default is _firstNoDefault:
-            raise ValueError("Iterable contained no elements (and no default value was given)")
-        else:
-            return default
+        pass
+    if default is _firstNoDefault:
+        raise ValueError("Iterable contained no elements (and no default value was given)")
+    else:
+        return default
 
 def emplaceInto(iterable1, iterable2):
     if isinstance(iterable2, dict):
