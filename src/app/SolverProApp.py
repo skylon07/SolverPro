20cbfbc0a05b5db3d11637bc76654e44e49788c2
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-07-27 00:24:28 -0600

    Add some basic/useless elements on screen

--- src/app/SolverProApp.py
+++ src/app/SolverProApp.py
@@ -1,7 +1,12 @@
 from textual.app import App
+from textual.widgets import Footer, Header, Label, Button
 
 class SolverProApp(App):
-    pass
+    def compose(self):
+        yield Header()
+        yield Label("Solver Pro!")
+        yield Button("Useless button")
+        yield Footer()
 
 if __name__ == "__main__":
     SolverProApp().run()
