70d754bc5d3c651b111ff4586c786b9ea130f861
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-07-29 13:45:44 -0600

    Add an input and history widget

--- src/app/SolverProApp.py
+++ src/app/SolverProApp.py
@@ -1,12 +1,33 @@
+from textual import events, on
 from textual.app import App
-from textual.widgets import Footer, Header, Label, Button
+from textual.screen import Screen
+from textual.widgets import Label, Header, TextLog, Input
+
+
+class MainScreen(Screen):
+    def compose(self):
+        yield TextLog()
+        yield Input(placeholder = " < Command >")
+        
+    def on_mount(self):
+        self.focus_next(Input)
+
+    @on(Input.Submitted)
+    def processCommand(self, event: Input.Submitted):
+        textLog = self.query_one(TextLog)
+        textLog.write(event.value)
+
+        input = self.query_one(Input)
+        input.value = ""
+
 
 class SolverProApp(App):
-    def compose(self):
-        yield Header()
-        yield Label("Solver Pro!")
-        yield Button("Useless button")
-        yield Footer()
+    SCREENS = {"main": MainScreen()}
+
+    def on_mount(self):
+        self.push_screen("main")
+
 
 if __name__ == "__main__":
-    SolverProApp().run()
+    app = SolverProApp()
+    app.run()
