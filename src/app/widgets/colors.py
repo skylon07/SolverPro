46cd7d6eff414329c4ec56ca288257269971e2b3
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-09-02 13:40:04 -0600

    Add some standardized colors

---
+++ src/app/widgets/colors.py
@@ -0,0 +1,24 @@
+from textual.color import Color
+
+
+class Colors:
+    background  = Color.parse("#202020")
+    fillPlain   = Color.parse("#303030")
+    fillBlue    = Color.parse("#37415a")
+    fillGreen   = Color.parse("#374b32")
+    fillRed     = Color.parse("#55323c")
+
+    textPlain   = Color.parse("#efefef")
+
+    borderPlain = Color.parse("#282828")
+    borderFocus = Color.parse("#0b78d2")
+    borderError = Color.parse("#0b78d2")
+
+    identifier  = Color.parse("#9fcff9")
+    operator    = Color.parse("#339ef4")
+    number      = Color.parse("#eea760")
+    alias       = Color.parse("#21c5c7")
+    punctuation = textPlain
+
+
+
